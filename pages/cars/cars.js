4f621c3b08c3743483a1187c3b81279f0e807134
plaul <lmor@kea.dk>
2023-09-25 13:31:37 +0200

    carspage

--- pages/cars/cars.js
+++ pages/cars/cars.js
@@ -1,6 +1,33 @@
 import { API_URL } from "../../settings.js"
+import { handleHttpErrors,sanitizeStringWithTableRows } from "../../utils.js"
 const URL = API_URL + "/cars/admin"
 
 export async function initCars() {
+
+  try {
+    const cars = await fetch(URL).then(handleHttpErrors)
+    //document.getElementById("table-rows").onclick = gotoToAddEditView
+    const carRows = cars.map(car => `
+<tr>
+<td>${car.id}</td>
+<td>${car.brand}</td>
+<td>${car.model}</td>
+<td>${car.pricePrDay}</td>
+<td>${car.bestDiscount}</td>
+<td><button id="${car.id}_column-id" class="btn btn-sm btn-secondary">Edit/delete</button> </td>      
+</tr>
+`).join("\n")
+
+    //You should ALWAYS do this from now on
+    const safeRows = sanitizeStringWithTableRows(carRows);
+    document.getElementById("table-rows").innerHTML = safeRows
+  } catch (err) {
+    if (err.apiError) {
+      document.getElementById("error").innerText = err.apiError.message
+    } else {
+      document.getElementById("error").innerText = err.message
+      console.error(err.message)
+    }
+  }
 }
 
